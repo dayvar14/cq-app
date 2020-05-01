2108971daf95121f8651cf46a8647c33ff956e28
Daniel Ayvar <dayvar@neiu.edu>
2020-04-30 21:50:04 -0500

    added test cases

---
+++ test_client_cases.sh
@@ -0,0 +1,11 @@
+python client/test_client.py "How much does a Macbook Air Cost?"
+python client/test_client.py "Where can i get a drink of water?"
+python client/test_client.py "Who is Don Cornero?"
+python client/test_client.py "How are you feeling today?"
+python client/test_client.py "What does a cake recipe consist of?"
+python client/test_client.py "When does the move release in theaters?"
+python client/test_client.py "Where is the Sears Tower?"
+python client/test_client.py "Who is Iron Man?"
+python client/test_client.py "What is the powerhouse of the cell?"
+python client/test_client.py "What inspired Micheal Jordan?"
+
